7a0dfd9fd8b307b66fc4cf7e4011e5fab3ef9a21
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-11 09:12:15 -0300

    Usando o for

--- src/LeituraDeDados.java
+++ src/LeituraDeDados.java
@@ -4,21 +4,23 @@ public class LeituraDeDados {
     public static void main(String[] args) {
         System.out.println("Bem vindo a classe LeituraDeDados");
 
-        Scanner leitura = new Scanner(System.in);
+        for (int i = 0; i < 2; i++) {
+            Scanner leitura = new Scanner(System.in);
 
-        System.out.println("Qual seu nome? ");
-        String nome = leitura.nextLine();
+            System.out.println("Qual seu nome? ");
+            String nome = leitura.nextLine();
 
-        System.out.println("Qual sua trilha de formação? ");
-        String formacao = leitura.nextLine();
+            System.out.println("Qual sua trilha de formação? ");
+            String formacao = leitura.nextLine();
 
-        System.out.println("Qual sua avaliação do processo de formação (0-10)? ");
-        int avaliacao = leitura.nextInt();
+            System.out.println("Qual sua avaliação do processo de formação (0-10)? ");
+            int avaliacao = leitura.nextInt();
 
-        System.out.println("""
+            System.out.println("""
                 Nome do estagiário: %s
                 Trilha de formação: %s
                 Avaliação de trilha: %d
                 """.formatted(nome, formacao, avaliacao));
+        }
     }
 }
