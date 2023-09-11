b7c296f2c0c9afbe751951927ad6686523897dde
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-11 07:47:40 -0300

    Aprendendo os tipos de variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bem vindo ao meu catálogo de filmes!");
+        System.out.println("Filme: Mãe");
+
+        int anoDeLancamento = 2043;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+        boolean incluidoNoPlano = true;
+        System.out.println("Incluso no plano: " + incluidoNoPlano);
+
+        double avaliacao = 4.75;
+        System.out.println("Nota média da crítica: " + avaliacao);
     }
 }
